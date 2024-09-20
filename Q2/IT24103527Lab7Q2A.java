f6e3780094d4e6ae73acf0f9b1d9d6acb663eb99
IT24103527 <it24103527@my.sliit.lk>
2024-09-20 10:18:50 +0530

    Add IT24103527Lab7Q2A.java to Q2A

---
+++ Q2/IT24103527Lab7Q2A.java
@@ -0,0 +1,15 @@
+public class IT24103527Lab7Q2A  {
+public static void main(String[] args)  {
+//Declaring variables 
+       int i , j ; 
+//Looping statements 
+    for(j=1 ; j<=4 ; j++)
+   {
+        for(i=1 ; i<=5 ; i++)
+  {
+        System.out.print("$") ;
+  }
+        System.out.println("\n") ; 
+    }
+}
+}
